9605989b08cc3627b3c02bc6ae122064896d1f86
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-17 19:41:50 -0600

    added

---
+++ NaviationLifeCircle/MAUXANHViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MAUXANHViewController.swift
+//  NaviationLifeCircle
+//
+//  Created by Kien Nguyen on 2021-05-17.
+//
+
+import UIKit
+
+class MAUXANHViewController: UIViewController {
+
+    var hoten: String!
+    
+    
+    @IBOutlet weak var txtHoTenXanh: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("🦋🦋🦋🦋🦋🦋🦋")
+        
+        txtHoTenXanh.text = hoten
+        
+        print("View 2 - Xanh : Did Load ")
+    }
+    
+
+    // after viewdidload, view Will appear :
+        override func viewWillAppear(_ animated: Bool) {
+            
+            print(" View 2 -Xanh: Will Appear ")
+        }
+        
+        
+    override func viewDidAppear(_ animated: Bool) {
+        
+        print(" View 2 -Xanh : Did Appear")
+    }
+
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        print(" View 2 -Xanh : Will DisAppear ")
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        print(" View 2 - Xanh : Did DisAppear ")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NaviationLifeCircle/ViewController.swift
+++ NaviationLifeCircle/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var txtHoTenVang: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       print(" View 1 - Vang : Did Load")
     }
-
+// after viewdidload, view Will appear :
+    override func viewWillAppear(_ animated: Bool) {
+        
+        print(" View 1 - Vang : Will appear ")
+    }
+    
+// after appear, disappear function :
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        print(" View 1 - Vang : Did Appear")
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        print(" View 1 - Vang : Will DisAppear ")
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        print(" View 1 : Did DisAppear ")
+    }
+    
+    
+    @IBAction func GOTO_XANH(_ sender: Any) {
+        
+        // Create variable for a storyboard:
+        
+        let sb = UIStoryboard( name: "Main", bundle: nil)
+        
+        
+        // Tao man hinh 2 mau xanh :
+        
+        let manhinh2 = sb.instantiateViewController(identifier: "MAUXANH") as! MAUXANHViewController
+        
+        //manhinh2.hoten=" Kelly & Kenny"
+        
+        // or nhap  truc tiep vo box o man hinh 1 :
+        
+        manhinh2.hoten = txtHoTenVang.text
+        
+        // Navigation PUSH (dua len san khau) or POP(duoi ra):
+        
+        self.navigationController?.pushViewController(manhinh2, animated: true)
+    }
+    
 
 }
 
